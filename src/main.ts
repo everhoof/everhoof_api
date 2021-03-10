2fa44c9b926a3fed9ca354212145f380bd1f30ed
intelrug <intelrug@gmail.com>
2021-03-10 15:00:16 +0300

    feat: move from socket.io to ws

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { WsAdapter } from '@nestjs/platform-ws';
 
 async function bootstrap() {
   const PORT = parseInt(process.env.PORT || '7700', 10);
 
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.useWebSocketAdapter(new WsAdapter());
   await app.listen(PORT);
 }
 bootstrap();
--- src/modules/tracks/tracks.gateway.ts
+++ src/modules/tracks/tracks.gateway.ts
@@ -1,7 +1,7 @@
 import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { forwardRef, Inject } from '@nestjs/common';
 import { TracksService } from '@modules/tracks/tracks.service';
-import { Server, Socket } from 'socket.io';
+import { Server } from 'ws';
 
 @WebSocketGateway(5501)
 export class TracksGateway {
@@ -12,13 +12,21 @@ export class TracksGateway {
 
   @WebSocketServer() server: Server;
 
-  async handleConnection(client: Socket): Promise<void> {
+  async handleConnection(client: WebSocket): Promise<void> {
     this.sendCurrentTrack(client);
   }
 
-  sendCurrentTrack(client?: Socket): void {
+  sendCurrentTrack(client?: WebSocket): void {
     const track = this.tracksService.getGatewayTrack();
     if (!track) return;
-    (client ?? this.server).emit('current-track', this.tracksService.getGatewayTrack());
+
+    const data = JSON.stringify({ event: 'current-track', ...track });
+    if (client) {
+      client.send(data);
+    } else {
+      this.server.clients.forEach((client) => {
+        client.send(data);
+      });
+    }
   }
 }
