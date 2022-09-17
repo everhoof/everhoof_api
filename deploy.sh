fd45e853fe9c965b526dd9304360a300108da6a2
intelrug <intelrug@gmail.com>
2022-09-17 18:44:59 +0300

    ci: implement deploy

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+export NVM_DIR=~/.nvm
+source ~/.nvm/nvm.sh
+
+find . -maxdepth 1 ! -name node_modules ! -name artifacts.tgz ! -name .env.local ! -name . ! -name .. -exec rm -rf {} \;
+tar -xvf artifacts.tgz
+rm artifacts.tgz
+
+pm2 stop ecosystem.config.js
+nvm exec 14.18.1 npm i -g yarn
+nvm exec 14.18.1 yarn --frozen-lockfile --production
+pm2 start ecosystem.config.js
+pm2 save
